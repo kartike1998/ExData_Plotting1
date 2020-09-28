de2f8b811bc009968b3512e9e4a47351785dcd9c
Kartike Bhardwaj <68057334+kartike1998@users.noreply.github.com>
2020-09-29 04:17:06 +0530

    Updated plotting method

--- plot3.R
+++ plot3.R
@@ -10,8 +10,8 @@ data$`Date/Time` <- as.POSIXct(data$`Date/Time`, format = "%d/%m/%Y %H:%M:%OS")
 png("plot3.png")
 plot(data$`Date/Time`, data$Sub_metering_1, type = "l",
      ylab = "Energy sub metering", xlab = "")
-points(data$`Date/Time`, data$Sub_metering_2, type = "l", col = "red")
-points(data$`Date/Time`, data$Sub_metering_3, type = "l", col = "blue")
+lines(data$`Date/Time`, data$Sub_metering_2, col = "red")
+lines(data$`Date/Time`, data$Sub_metering_3, col = "blue")
 legend("topright", lty = "solid", col = c("black", "red", "blue"),
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
